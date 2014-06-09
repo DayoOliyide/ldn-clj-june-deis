9cf9a9894713d76b790de411f1e4963b8b7b6052
Matt Valentine-House <matt@eightbitraptor.com>
2014-06-09 20:50:29 +0100

    add min lein version to project
    In an effort to stop deis complaining.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
+  :min-lein-version "2.0.0"
   :ring {:handler ldn-clj-june-deis.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
